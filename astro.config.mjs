52d11deceb691d42409ecd4b29be2eb0dc192cc5
Petrovici Catalin-Cristian <73588411+catalinpetrovici24@users.noreply.github.com>
2023-03-28 22:24:03 +0300

    Responsive images & interaction. Light/Dark Mode adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
+
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    // compress({
+    //   css: false,
+    //   html: false,
+    //   js: false,
+    //   svg: false,
+    //   png: false,
+    // }),
+    robotsTxt(),
+    // image(),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgDark: 'var(--bg-color-dark)',
+        fontDark: 'var(--font-color-dark)',
+        fontDarkGray1: 'var(--font-color-dark-gray-1)',
+        fontDarkGray2: 'var(--font-color-dark-gray-2)',
+        fontDarkGray3: 'var(--font-color-dark-gray-3)',
+        fontDarkGray4: 'var(--font-color-dark-gray-4)',
+        fontDarkGray5: 'var(--font-color-dark-gray-5)',
+        fontLightGray1: 'var(--font-color-light-gray-1)',
+        fontLightGray2: 'var(--font-color-light-gray-2)',
+        fontLightGray3: 'var(--font-color-light-gray-3)',
+        bgLight: 'var(--bg-color-light)',
+        fontLight: 'var(--font-color-light)',
+        btnDark: 'var(--btn-bg-color-dark)',
+        btnLight: 'var(--btn-bg-color-light)',
+        btnFont: 'var(--btn-font-color)',
+        light: 'var(--accent-light)',
+        dark: 'var(--accent-dark)',
+      },
+    },
   },
   plugins: [],
 };
