dabea3f511e3bc2628514753c9a6e3c252c34961
Petrovici Catalin-Cristian <73588411+catalinpetrovici24@users.noreply.github.com>
2023-03-30 21:59:28 +0300

    PWA Ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
-import svelte from '@astrojs/svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
 import sitemap from '@astrojs/sitemap';
+
+import manifest from './src/utils/manifest.ts';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://catalinpetrovici.com',
   integrations: [
-    svelte(),
     tailwind(),
     // compress({
     //   css: false,
@@ -22,7 +23,50 @@ export default defineConfig({
     robotsTxt(),
     sitemap(),
   ],
-  // server: {
-  //   port: 3001
-  // }
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        workbox: {
+          // Don't fallback on document based (e.g. `/some-page`) requests
+          // This removes an errant console.log message from showing up.
+          navigateFallback: null,
+        },
+        manifest: {
+          name: 'Petrovici Catalin',
+          short_name: 'Petrovici Catalin',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#0A0915',
+          theme_color: '#ffffff',
+          lang: 'en',
+          scope: '/',
+          icons: [
+            {
+              src: './favicon/favicon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: './favicon/favicon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+            {
+              src: './favicon/favicon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: './favicon/favicon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
+  },
 });
