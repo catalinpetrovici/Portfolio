335888775af525e082890713d3551ea2f8c5bf11
Petrovici Catalin-Cristian <73588411+catalinpetrovici24@users.noreply.github.com>
2023-04-01 00:43:02 +0300

    Refactoring & LogoSection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
 import { VitePWA } from 'vite-plugin-pwa';
-
 import sitemap from '@astrojs/sitemap';
-
-import manifest from './src/utils/manifest.ts';
+import { manifest } from './src/utils/manifest';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://catalinpetrovici.com',
-  integrations: [
-    tailwind(),
-    // compress({
-    //   css: false,
-    //   html: false,
-    //   js: false,
-    //   svg: false,
-    //   png: false,
-    // }),
-    robotsTxt(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), robotsTxt(), sitemap()],
   vite: {
     plugins: [
       VitePWA({
@@ -32,40 +18,7 @@ export default defineConfig({
           // This removes an errant console.log message from showing up.
           navigateFallback: null,
         },
-        manifest: {
-          name: 'Petrovici Catalin',
-          short_name: 'Petrovici Catalin',
-          start_url: '/',
-          display: 'standalone',
-          background_color: '#0A0915',
-          theme_color: '#ffffff',
-          lang: 'en',
-          scope: '/',
-          icons: [
-            {
-              src: './favicon/favicon-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: './favicon/favicon-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-            {
-              src: './favicon/favicon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: './favicon/favicon-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
-        },
+        manifest,
       }),
     ],
   },
--- src/utils/manifest.ts
+++ src/utils/manifest.ts
@@ -1,30 +1,36 @@
-// Type imports
 import type { ManifestOptions } from 'vite-plugin-pwa';
 
 /**
- * Defines the configuration for PWA webmanifest.
+ * Configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
   name: 'Petrovici Catalin',
   short_name: 'Petrovici Catalin',
-  description:
-    'Petrovici Catalin is a Personal website portfolio to showcase projects and personal information.',
-  theme_color: '#0A0915', // Change this to your primary color.
-  background_color: '#0A0915', // Change this to your background color.
-  display: 'minimal-ui',
+  start_url: '/',
+  display: 'standalone',
+  background_color: '#0A0915',
+  theme_color: '#ffffff',
+  lang: 'en',
+  scope: '/',
   icons: [
     {
-      src: './favicons/favicon-192x192.png',
+      src: './favicon/favicon-192x192.png',
       sizes: '192x192',
       type: 'image/png',
     },
     {
-      src: './favicons/favicon-512x512.png',
+      src: './favicon/favicon-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'any maskable',
+    },
+    {
+      src: './favicon/favicon-512x512.png',
       sizes: '512x512',
       type: 'image/png',
     },
     {
-      src: './favicons/favicon-512x512.png',
+      src: './favicon/favicon-512x512.png',
       sizes: '512x512',
       type: 'image/png',
       purpose: 'any maskable',
