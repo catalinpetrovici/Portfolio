332f707f36e6cbdd85fa84958263c05def097556
Petrovici Catalin-Cristian <73588411+catalinpetrovici24@users.noreply.github.com>
2023-04-01 20:41:38 +0300

    Resources and Contact Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,34 +4,32 @@ import robotsTxt from 'astro-robots-txt';
 import { VitePWA } from 'vite-plugin-pwa';
 import sitemap from '@astrojs/sitemap';
 import { manifest } from './src/utils/manifest';
-
 import partytown from '@astrojs/partytown';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://catalinpetrovici.com',
-  // server: { port: 80, host: true },
-  integrations: [
-    tailwind(),
-    robotsTxt(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        workbox: {
-          // Don't fallback on document based (e.g. `/some-page`) requests
-          // This removes an errant console.log message from showing up.
-          navigateFallback: null,
-        },
-        manifest,
-      }),
-    ],
+  site: 'http://192.168.0.111',
+  server: {
+    port: 80,
+    host: true
   },
+  // site: 'https://catalinpetrovici.com',
+  integrations: [tailwind(), robotsTxt(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), preact()],
+  vite: {
+    plugins: [VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      },
+      manifest
+    })]
+  }
 });
